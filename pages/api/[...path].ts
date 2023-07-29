2741be3ea1decc335028cccf287d5c1de5dea11c
Le Dinh <lenguyennhutdinh@gmail.com>
2023-07-29 10:52:22 +0700

    set up api

---
+++ pages/api/[...path].ts
@@ -0,0 +1,22 @@
+import httpProxy from 'http-proxy'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const baseUrl = process.env.API_URL
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const proxy = httpProxy.createProxyServer()
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  req.headers.cookie = ''
+
+  proxy.web(req, res, {
+    target: baseUrl,
+    changeOrigin: true,
+    selfHandleResponse: false,
+  })
+}
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -3,18 +3,17 @@ import { Pagination, Post } from '@/models'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data =
-	| {
-			data: Post[]
-			pagination: Pagination
-	  }
-	| { name: string }
+  | {
+      data: Post[]
+      pagination: Pagination
+    }
+  | { name: string }
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
-) {
-	const response = await fetch('http://localhost:4000/api/products?_page=1')
-	const data = await response.json()
+const baseUrl = process.env.API_URL
 
-	res.status(200).json(data)
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const response = await fetch(`${baseUrl}/api/products?_page=1`)
+  const data = await response.json()
+
+  res.status(200).json(data)
 }
